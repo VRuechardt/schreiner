481c91345723c5cab8c6060a55ebaee341736758
VRuechardt <valentin@ruechardt.de>
2018-01-16 14:17:06 +0100

    did some stuff

--- backend/routes/example.js
+++ backend/routes/example.js
@@ -3,6 +3,22 @@ const router = new Router({
 	prefix: '/example'
 })
 
-router.post('/', async ctx => ctx.body = ctx.request.body.id)
+let obj = [
+	{
+		title: '',
+		isbn: '12367:234128',
+		description: 'Tolles Buch'
+	}
+]
+let arr = []
+arr.map(elem => {
+	return 2
+})
+
+const handle = (ctx) => {
+	ctx.body = obj
+}
+
+router.get('/', handle)
 
 module.exports = router.routes()
